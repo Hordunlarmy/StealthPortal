e7b111db28d9927b82f9f1a53eef4f6e2b20c967
hordunlarmy <hordunlarmy@gmail.com>
2024-04-02 14:26:32 +0000

    Add decryption functionality for AES encrypted messages

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from forms import RegistrationForm, LoginForm
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
 from base64 import b64decode
 from datetime import timedelta
 import uuid
@@ -44,18 +46,31 @@ def load_private_key():
         return None
 
 
-def decrypt_message(encrypted_message):
+def decrypt_key(encrypted_key):
     try:
         private_key = load_private_key()
         if private_key is None:
             return None
 
         cipher = PKCS1_OAEP.new(private_key, hashAlgo=SHA256)
-        decrypted_message = cipher.decrypt(b64decode(encrypted_message))
-        return decrypted_message.decode()
-    except ValueError as e:
-        print("Error decrypting message:", e)
+        decrypted_key_bytes = cipher.decrypt(b64decode(encrypted_key))
+        decrypted_key_str = decrypted_key_bytes.decode(
+            'utf-8')
+        print("The decrypted key-----------", decrypted_key_str)
+        return decrypted_key_str
+    except Exception as e:
+        print("Error decrypting key:", e)
         return None
+
+
+def decrypt_message(data: str, key: str) -> str:
+    secret_key = key
+    iv = "1020304050607080"
+    ciphertext = b64decode(data)
+    derived_key = b64decode(secret_key)
+    cipher = AES.new(derived_key, AES.MODE_CBC, iv.encode('utf-8'))
+    decrypted_data = cipher.decrypt(ciphertext)
+    return unpad(decrypted_data, 16).decode("utf-8")
 
 
 def generate_secret_word(length):
@@ -150,8 +165,12 @@ def handle_send_message(data):
     if client_id not in rooms:
         return
     encrypted_message = data["message"]
+    encrypted_key = data["key"]
+    print(f"[ENCRYPTED] key is {encrypted_key}")
     print(f"[ENCRYPTED] {client_id} said: {data['message']}")
-    decrypted_message = decrypt_message(encrypted_message)
+    print(f"[ENCRYPTED] message type is ... {type(encrypted_message)}")
+    decrypted_key = decrypt_key(encrypted_key)
+    decrypted_message = decrypt_message(encrypted_message, decrypted_key)
     emit('receive_message', {'message': decrypted_message}, room=room)
     print(f"[DECRYPTED] {client_id} said: {decrypted_message}")
 
